03d2dc2b70485eebaebab6644cc2707a93f1d2e5
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-22 21:11:33 +0200

    Call api

--- faucet/app/page.tsx
+++ faucet/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -16,9 +18,20 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <button
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            rel="noopener noreferrer"
+            onClick={() =>
+              fetch("/api", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ address: "0x" }),
+              }).then(async (res) => {
+                const json = await res.json();
+                alert(JSON.stringify(json, null, 2));
+              })
+            }
           >
-            Send request
+            Send request to backend
           </button>
         </div>
       </main>
