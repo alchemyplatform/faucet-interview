88deae67b1ae922f98ede6d25ef217d318d29404
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-22 21:12:01 +0200

    Format json

--- faucet/app/api/route.ts
+++ faucet/app/api/route.ts
@@ -4,5 +4,7 @@ export async function POST(request: Request) {
   const body = await request.json();
   console.log("Body:", body);
   console.log(process.env.ALCHEMY_API_KEY);
-  return NextResponse.json({ message: "Hello from the backend!" });
+  return NextResponse.json({
+    message: "Hello from the backend!",
+  });
 }
