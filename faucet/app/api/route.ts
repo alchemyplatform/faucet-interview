e47d419912dc31fc9615d3b73a725cb5ad0808de
Bastien <9058670+bmoyroud@users.noreply.github.com>
2024-10-22 21:11:39 +0200

    Log api key

--- faucet/app/api/route.ts
+++ faucet/app/api/route.ts
@@ -3,5 +3,6 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const body = await request.json();
   console.log("Body:", body);
+  console.log(process.env.ALCHEMY_API_KEY);
   return NextResponse.json({ message: "Hello from the backend!" });
 }
